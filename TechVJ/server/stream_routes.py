78a98a3f26f8d7a7fcf5bc55f5647fb2c35d86d4
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2024-11-27 20:10:56 +0530

    Update stream_routes.py

--- TechVJ/server/stream_routes.py
+++ TechVJ/server/stream_routes.py
@@ -6,12 +6,12 @@ import secrets
 import mimetypes
 from aiohttp import web
 from aiohttp.http_exceptions import BadStatusLine
-from Techayu.bot import multi_clients, work_loads, StreamBot
-from Techayu.server.exceptions import FIleNotFound, InvalidHash
-from Techayu import StartTime, __version__
+from TechVJ.bot import multi_clients, work_loads, StreamBot
+from TechVJ.server.exceptions import FIleNotFound, InvalidHash
+from TechVJ import StartTime, __version__
 from ..utils.time_format import get_readable_time
 from ..utils.custom_dl import ByteStreamer
-from Techayu.utils.render_template import render_page
+from TechVJ.utils.render_template import render_page
 from config import MULTI_CLIENT
 
 
