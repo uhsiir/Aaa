171311f995826618319d5e863b687679f2309788
AYUSHRAJ5354n <167030308+AYUSHRAJ5354n@users.noreply.github.com>
2024-12-13 00:59:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "26107399"))
 API_HASH = environ.get("API_HASH", "8009803348")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8009803348:AAGEelK-")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8009803348:AAGEelK-mmHaALuXxSMZgyHMfmgStb1VZOc")
 
 PICS = (environ.get('PICS', 'https://www.freeiconspng.com/uploads/welcome-png-hd-17.png')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '1685470205 5212197608').split()]
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002206233283"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002471667282"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
